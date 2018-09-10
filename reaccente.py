a4f54ef0204256e9f7c86395caecac21b0e52087
cquest <cquest@openstreetmap.fr>
2018-09-10 18:11:48 +0200

    première version

---
+++ reaccente.py
@@ -0,0 +1,65 @@
+import sys
+import select
+import re
+from unidecode import unidecode
+
+
+def add_dico(mot, dico):
+    if mot < 'a':
+        return
+    maj = unidecode(mot).upper()
+    if maj in dico:
+        if mot not in dico[maj]:
+            dico[maj].append(mot)
+    else:
+        dico[maj] = [mot]
+
+
+def load_dico(fichier, dico):
+    "Charge le dictionnaire MAJUSCULE > minuscules accentuées"
+    with open(fichier, mode='r') as dicco:
+        for mot in dicco:
+            mot = mot.replace('\n', '')
+            m = re.sub(r'/.*', '', mot)
+            add_dico(m, dico)
+            # pluriels et variantes
+            pluriel = re.sub(r'.*/', '', mot)
+            if pluriel == 'X.':
+                add_dico(re.sub(r'l$', 'ux', m), dico)
+            elif pluriel == 'S.':
+                add_dico(re.sub(r'$', 's', m), dico)
+            elif pluriel == 'W.':
+                add_dico(re.sub(r'e$', '', m), dico)
+                add_dico(re.sub(r'e$', 'es', m), dico)
+                add_dico(re.sub(r'e$', 'ux', m), dico)
+    return dico
+
+
+def reaccente(maj):
+    for mot in maj.split():
+        if mot.lower() in articles:
+            maj = maj.replace(mot, mot.lower())
+        elif mot.upper() in dico and len(dico[mot.upper()]) == 1:
+            maj = maj.replace(mot, dico[mot.upper()][0].capitalize())
+        else:
+            maj = maj.replace(mot, mot.lower().capitalize())
+    return maj
+
+
+dico = dict()
+dico = load_dico('fr-toutesvariantes.dic', dico)
+dico = load_dico('complements.dic', dico)
+articles = ['le', 'la', 'les', 'un',  'une', 'des', 'au', 'du', 'de', 'aux']
+
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        if select.select([sys.stdin, ], [], [], 0.0)[0]:
+            lines = sys.stdin.readlines()
+            for l in lines:
+                print(reaccente(l.replace('\n', '')))
+        else:
+            print("""Usage:  reaccente.py texte ou fichier
+        reaccente.py 'BOULEVARD DU MARECHAL JEAN MARIE DE LATTRE DE TASSIGNY'
+        reaccente.py fichier.txt""")
+    else:
+        print(reaccente(sys.argv[1]))
