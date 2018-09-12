51a776dc5ee1110143c1dc059bf8192dde037403
cquest <cquest@openstreetmap.fr>
2018-09-12 17:16:40 +0200

    suffixes générés à partir des fichiers ".aff" d'affixes

--- reaccentue.py
+++ reaccentue.py
@@ -9,7 +9,7 @@ from unidecode import unidecode
 
 
 def add_dico(mot, dico):
-    if mot < 'a':
+    if mot < 'A':
         return
     maj = unidecode(mot).upper()
     if maj in dico:
@@ -19,29 +19,61 @@ def add_dico(mot, dico):
         dico[maj] = [mot]
 
 
+def load_word(mot, dico, affixes):
+    m = re.sub(r'/.*', '', mot)
+    add_dico(m, dico)
+    rules = re.sub(r'.*/', '', mot)
+    while rules != '':
+        rule = rules[0:2]
+        rules = rules[2:]
+        if rule in affixes:
+            if affixes[rule]['type'] == 'SFX':
+                for r in affixes[rule]['rules']:
+                    if re.search(r[4]+'$', m):
+                        suffixe = re.sub(r'/.*', '', re.sub(r[2]+'$', r[3], m))
+                        if suffixe != m:
+                            add_dico(suffixe, dico)
+                        if '/' in r[3]:
+                            load_word(re.sub(r[2]+'$', r[3], m), dico, affixes)
+            if affixes[rule]['type'] == 'PFX':
+                for r in affixes[rule]['rules']:
+                    if re.search('^'+r[4], m):
+                        if '/' in r[3]:
+                            add_dico(re.sub(r'/.*', '', re.sub('^'+r[2],
+                                            r[3], m)), dico)
+                            load_word(re.sub('^'+r[2], r[3], m), dico, affixes)
+                        elif re.sub('^'+r[2], r[3], m) != m:
+                            add_dico(re.sub('^'+r[2], r[3], m), dico)
+                            print('-> ', re.sub('^'+r[2], r[3], m), r)
+
+
 def load_dico(fichier, dico):
     "Charge le dictionnaire MAJUSCULE > minuscules accentuées"
-    with open(fichier, mode='r') as dicco:
+
+    # charge les définitions des suffixes (SFX)
+    affixes = dict()
+    try:
+        with open(fichier+'.aff', mode='r') as affix:
+            for aff in affix:
+                af = aff.split()
+                if len(af) == 4 and af[0] in ['SFX']:  # header
+                    affixes[af[1]] = {'type': af[0], 'cross_product': af[2],
+                                      'rules': []}
+                if len(af) > 4 and af[0] in ['SFX']:  # rules
+                    if af[2] == '0':
+                        af[2] = ''
+                    if af[3][0] == '0':
+                        af[3] = af[3][1:]
+                    affixes[af[1]]['rules'].append(af)
+    except:
+        pass
+
+    # charge le contenu du dictionaire en appliquant préfixes/suffixes
+    with open(fichier+'.dic', mode='r') as dicco:
         for mot in dicco:
             mot = mot.replace('\n', '')
-            m = re.sub(r'/.*', '', mot)
-            add_dico(m, dico)
-            # pluriels et variantes
-            pluriel = re.sub(r'.*/', '', mot)
-            if pluriel == 'X.':
-                add_dico(re.sub(r'i?l$', 'ux', m), dico)
-                add_dico(re.sub(r'[aeoœ]u$', '\1x', m), dico)
-            elif pluriel in ['S.', 'S=']:
-                add_dico(re.sub(r'[^sxz]$', '\1s', m), dico)
-            elif pluriel in ['I.']:
-                add_dico(re.sub(r'[^sxz]$', '\1s', m), dico)
-                add_dico(re.sub(r'a$', 'e', m), dico)
-                add_dico(re.sub(r'([eo]|um)$', 'i', m), dico)
-                add_dico(re.sub(r'um$', 'a', m), dico)
-            elif pluriel == 'W.':
-                add_dico(re.sub(r'e$', '', m), dico)
-                add_dico(re.sub(r'e$', 'es', m), dico)
-                add_dico(re.sub(r'e$', 'ux', m), dico)
+            load_word(mot, dico, affixes)
+
     return dico
 
 
